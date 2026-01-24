34d3be7a9251fcadfd7218dac110c92d10424f4b
Niklas Fiekas <niklas.fiekas@backscattering.de>
2026-01-24 19:45:06 +0100

    expose PARTITION_THRESHOLD

--- src/lib.rs
+++ src/lib.rs
@@ -74,8 +74,11 @@ where
 /// 2. Every element in `v[start..end]`, which is sorted, and smaller than or equal to
 /// 3. Every element in `v[end..]`.
 #[inline]
-pub fn partial_sort<T, F, R>(v: &mut [T], range: R, mut is_less: F)
-where
+pub fn partial_sort<T, F, R, const PARTITION_THRESHOLD: usize>(
+    v: &mut [T],
+    range: R,
+    mut is_less: F,
+) where
     F: FnMut(&T, &T) -> bool,
     R: RangeBounds<usize>,
 {
@@ -104,7 +107,6 @@ where
     // A heuristic factor to decide whether to partition the slice or not.
     // If the range bound is close to the edges of the slice, it's not worth
     // partitioning first.
-    const PARTITION_THRESHOLD: usize = 8;
     let mut v = v;
     if end + PARTITION_THRESHOLD <= len {
         v = partition_at_index(v, end - 1, &mut is_less).0;
--- tests/partial_sort.rs
+++ tests/partial_sort.rs
@@ -3,6 +3,6 @@ use partial_sort_bench::partial_sort;
 #[test]
 fn test_partial_sort() {
     let mut v = [1, 2, 3, 1, 2, 3, 4, 0];
-    partial_sort(&mut v, ..3, |a, b| a.lt(b));
+    partial_sort::<_, _, _, 8>(&mut v, ..3, |a, b| a.lt(b));
     assert_eq!(v[..3], [0, 1, 1]);
 }
